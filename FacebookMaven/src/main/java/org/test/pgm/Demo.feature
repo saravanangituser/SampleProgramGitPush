7c306f39f4559f8af72a44b0263271aa66c684fd
user <user@hp>
2021-08-11 21:39:35 +0530

    Initial commit

---
+++ FacebookMaven/src/main/java/org/test/pgm/Demo.feature
@@ -0,0 +1,5 @@
+Feature:Test the login page
+Scenario:
+Given:User is in login page
+When:User wants to enter credentials
+Then:User click login button
---
+++ FacebookMaven/src/main/java/org/test/pgm/TestingProgram.java
@@ -0,0 +1,12 @@
+package org.test.pgm;
+
+public class TestingProgram {
+	public static void main(String args[])
+	{
+		System.out.println("Testing completed");
+	}
+
+
+
+
+}
